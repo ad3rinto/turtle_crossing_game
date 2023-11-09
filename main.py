cf71d0e252dcd62be840d759b9201056354b5387
ad3rinto <aad3rinto@gmail.com>
2023-11-09 13:31:20 +0000

    solved player on screen and player move challenge

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+# Create a turtle player that starts at the bottom of the screen and listen for the "Up" keypress to
+# move the turtle north. If you get stuck, check the video walkthrough in Step 3.
+player = Player()
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    # listen to up kep press
+    screen.listen()
+    screen.onkey(player.move, "Up")
